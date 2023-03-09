0a1aa2c3b16d1d434d331348a30c5c648e8f6fc1
HumbertodeCastro <h.decastro@outlook.com>
2023-03-09 10:33:14 +0100

    Game

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -15,6 +16,6 @@ public class Main {
             System.out.println("Welkom!");
         }
 
-
+        System.out.println("Heyyy je bent mooit watsjenaam");
     }
 }
