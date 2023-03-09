604679bbd1d484b3ac28e5b5c7210861b1892eb2
HumbertodeCastro <h.decastro@outlook.com>
2023-03-09 10:05:36 +0100

    awo

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         int nine = 69;
         System.out.println("Graige bakoba master"+nine);
+
+        System.out.println("KFC blockparty");
+
     }
 }
