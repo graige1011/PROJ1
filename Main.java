2bf5e8e76f45c242de0d2d505c65ef488d0655b7
graige1011 <120499671+graige1011@users.noreply.github.com>
2023-03-10 11:34:21 -0400

    object ge add

--- Main.java
+++ Main.java
@@ -1,24 +1,31 @@
 
 import java.sql.SQLOutput;
 import java.util.Scanner;
+class Pizza{
+    String naam;
+    public Pizza(String naam){
+        this.naam = naam;
+    }
+}
 public class Main {
     public static void main(String[] args) {
-
+        Pizza pizza1 = new Pizza("BBC express");
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println(pizza1.naam);
         System.out.println("Voer uw naam in");
         String naam = scanner.nextLine();
         if(naam.equals("Graige") || naam.equals("Graige"))
         {
             System.out.println("Graige pretu please bai foi djaki");
         }
-        if(naam.equals("Keanu"){
+        if(naam.equals("Keanu")){
             System.out.println("Bai laba bo wok");
         }
         else {
+
             System.out.println("Welkom!");
         }
 
-        System.out.println("Heyyy je bent mooit watsjenaam");
+        System.out.println("Heyyy je bent mooi watsjenaam");
     }
 }
