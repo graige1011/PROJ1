7e6b5b6ffbc50ddb1f08da65b52f6ca2f0b969ef
Luciano <lkmlfliermans@gmail.com>
2023-03-21 14:48:36 +0100

    Bullshit verwijderd

--- Klant.java
+++ Klant.java
@@ -1,2 +1,20 @@
-public class Klant {
+public abstract class Klant {
+    String voorNaam;
+    String achterNaam;
+    String email;
+
+}
+
+class Particulier extends Klant {
+    double particuliersKorting = 0.90;
+
+}
+
+class Overheid extends Klant {
+    double overheidsKorting = 1.00;
+
+}
+class Bedrijf extends Klant {
+    double bedrijfsKorting = 0.80;
+
 }
