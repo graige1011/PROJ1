149943dbab476e9892a5b87cfc29c67e780d16b5
graige1011 <120499671+graige1011@users.noreply.github.com>
2023-03-22 15:22:45 -0400

    offerte class v3 edits

--- graigedeel.java
+++ graigedeel.java
@@ -1,12 +1,12 @@
 class Offerte{
     int offertenr;
-    Onderdeel onderdeel;
+    Onderdeel3 onderdeel;
     String datum;
     String bedrijfsnaam;
     String adres;
     int telefoonnr;
 
-    public Offerte(int offertenr, Onderdeel onderdeel, String datum, String bedrijfsnaam, String adres, int telefoonnr) {
+    public Offerte(int offertenr, Onderdeel3 onderdeel, String datum, String bedrijfsnaam, String adres, int telefoonnr) {
         this.offertenr = offertenr;
         this.onderdeel = onderdeel;
         this.datum = datum;
@@ -23,7 +23,7 @@ class Offerte{
         this.offertenr = offertenr;
     }
 
-    public Onderdeel getOnderdeel() {
+    public Onderdeel3 getOnderdeel() {
         return onderdeel;
     }
 
@@ -63,8 +63,29 @@ class Offerte{
         this.telefoonnr = telefoonnr;
     }
 }
+class Onderdeel3 {
+    String naam;
+    String omschrijving;
+    int aantal;
+    double stukprijs;
+    double totaal;
+
+    public Onderdeel3(String naam, String omschrijving, int aantal, double stukprijs, double totaal){
+        this.naam = naam;
+        this.omschrijving = omschrijving;
+        this.aantal = aantal;
+        this.stukprijs = stukprijs;
+        this.totaal = totaal;
+    }
+}
 public class graigedeel {
     public static void main(String[] args) {
-        Offerte offerte1 =  new Offerte(001,onderdeel,"22-03-23","waterworks","andijlaan 21",0616505007);
+        Onderdeel3 roer = new Onderdeel2("Roer","Carbon fibre ",1,799.99,799.99);
+        Offerte offerte1 =  new Offerte(001,Onderdeel3,"22-03-23","waterworks","andijlaan 21",0616505007);
+        
+        System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", "Onderdeelnaam", "Omschrijving", "Aantal", "Stukprijs", " Totaal");
+        System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", "-------------", "---------------", "------", "-----------", "-----------");
+        System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", roer.naam, roer.omschrijving, roer.aantal, roer.stukprijs, roer.totaal);
+
     }
 }
