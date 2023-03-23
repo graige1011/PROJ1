f3b269132b286369c7f412e13df9ca605cda2200
graige1011 <120499671+graige1011@users.noreply.github.com>
2023-03-23 05:26:23 -0400

    offerte class v5 edits

--- Graigedeel.java
+++ Graigedeel.java
@@ -5,14 +5,15 @@ class Offerte{
     String bedrijfsnaam;
     String adres;
     int telefoonnr;
+    double totaal;
 
-    public Offerte(int offertenr, String datum, String bedrijfsnaam, String adres, int telefoonnr) {
+    public Offerte(int offertenr, String datum, String bedrijfsnaam, String adres, int telefoonnr,double totaal) {
         this.offertenr = offertenr;
-
         this.datum = datum;
         this.bedrijfsnaam = bedrijfsnaam;
         this.adres = adres;
         this.telefoonnr = telefoonnr;
+        this.totaal = totaal;
     }
 
     public int getOffertenr() {
@@ -68,24 +69,36 @@ class Onderdeel3 {
     String omschrijving;
     int aantal;
     double stukprijs;
-    double totaal;
+    double stuktotaal;
 
-    public Onderdeel3(String naam, String omschrijving, int aantal, double stukprijs, double totaal){
+    public Onderdeel3(String naam, String omschrijving, int aantal, double stukprijs, double stuktotaal){
         this.naam = naam;
         this.omschrijving = omschrijving;
         this.aantal = aantal;
         this.stukprijs = stukprijs;
-        this.totaal = totaal;
+        this.stuktotaal=stuktotaal;
     }
 }
 public class Graigedeel {
     public static void main(String[] args) {
-        Onderdeel3 roer = new Onderdeel3("Roer","Carbon fibre ",1,799.99,799.99);
-        Offerte offerte1 =  new Offerte(1,"22-03-23","waterworks","andijlaan 21",616505007);
+        Onderdeel3 roer = new Onderdeel3("Roer","Carbon fibre ",1,799.99,1249.99);
+        Offerte offerte1 =  new Offerte(157,"22-03-23","waterworks","andijlaan 21",616505007,859.99);
+        System.out.println("Offerte Nr: "+ offerte1.offertenr);
+        System.out.println("");
+        System.out.println("Datum: "+ offerte1.datum);
+        System.out.println("");
+        System.out.println("Bedrijfsnaam: "+ offerte1.bedrijfsnaam);
+        System.out.println("");
+        System.out.println("Adres: "+ offerte1.adres);
+        System.out.println("");
+        System.out.println("");
+
+
+
 
         System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", "Onderdeelnaam", "Omschrijving", "Aantal", "Stukprijs", " Totaal");
         System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", "-------------", "---------------", "------", "-----------", "-----------");
-        System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", roer.naam, roer.omschrijving, roer.aantal, roer.stukprijs, roer.totaal);
+        System.out.printf("%-15s %-20s %-10s %-15s %-15s\n", roer.naam, roer.omschrijving, roer.aantal, roer.stukprijs, roer.stuktotaal);
 
     }
 }
