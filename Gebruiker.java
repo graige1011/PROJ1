31590b7d23466280fd175c34b05ed530ee6fb84b
HumbertodeCastro <h.decastro@outlook.com>
2023-04-05 14:08:06 +0200

    uitcomment

--- Gebruiker.java
+++ Gebruiker.java
@@ -1,24 +1,25 @@
-public class Gebruiker {
-        private String gebruikersnaam;
-        private String wachtwoord;
-        private String rol;
 
-        public Gebruiker(String gebruikersnaam, String wachtwoord, String rol) {
-            this.gebruikersnaam = gebruikersnaam;
-            this.wachtwoord = wachtwoord;
-            this.rol = rol;
-        }
-
-        public String getGebruikersnaam() {
-            return gebruikersnaam;
-        }
-
-        public String getWachtwoord() {
-            return wachtwoord;
-        }
-
-        public String getRol() {
-            return rol;
-        }
-    }
+//public class Gebruiker {
+//        private String gebruikersnaam;
+//        private String wachtwoord;
+//        private String rol;
+//
+//        public Gebruiker(String gebruikersnaam, String wachtwoord, String rol) {
+//            this.gebruikersnaam = gebruikersnaam;
+//            this.wachtwoord = wachtwoord;
+//            this.rol = rol;
+//        }
+//
+//        public String getGebruikersnaam() {
+//            return gebruikersnaam;
+//        }
+//
+//        public String getWachtwoord() {
+//            return wachtwoord;
+//        }
+//
+//        public String getRol() {
+//            return rol;
+//        }
+//    }
 
